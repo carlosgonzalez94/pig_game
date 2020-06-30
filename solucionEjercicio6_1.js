a5948d785aea8639a6da7a7f6d71dce44d8cb840
carlos-gonzalez <calichemga@gmail.com>
2020-06-30 18:19:59 -0500

    pig game with html, css and javascript, instructions in readme

---
+++ solucionEjercicio6_1.js
@@ -0,0 +1,162 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores, roundScore, activePlayer, gamePlaying;
+
+init();
+
+var lastDice;
+
+
+//document.querySelector('#current-' + activePlayer).textContent = dice ;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice +'</em>';
+/* van a la funcion init()
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+
+document.querySelector('.dice').style.display = 'none';*/
+
+// function btn(){
+//     //hacer algo aqui
+// }
+// btn();
+
+//para llamar a una funcion externa
+// document.querySelector('.btn-roll').addEventListener('click', btn);
+
+//usar una funcion anonima que solo podra usarse en este conexto
+// document.querySelector('.btn-roll').addEventListener('click', function() {
+//     //hacer algo aqui
+// });
+/* va a la funcion init
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';*/
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    if(gamePlaying) {
+
+        //1. random number
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
+
+        //2. mostrar el resultado
+        document.getElementById('dice-1').style.display = 'block';
+        document.getElementById('dice-2').style.display = 'block';
+        document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+        document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
+
+
+
+        //3. actualizar el nuevo valor si el valor al girar no es 1
+        /*if(dice === 6 && lastDice ===6) {
+            //el jugador lo pierde todo
+            scores[activePlayer] = 0;
+            document.querySelector('#score-' + activePlayer).textContent = '0';
+            nextPlayer();
+        }else if(dice !== 1){
+            //agregar puntaje
+            roundScore += dice;
+            // roundScore = roundScore + dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        }else{
+            nextPlayer();
+        }
+        lastDice = dice;*/
+        if(dice1 !== 1 && dice2 !==1 ){
+            //agregar puntaje
+            roundScore += dice1 + dice2;
+            // roundScore = roundScore + dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        }else{
+            nextPlayer();
+        }
+    }
+    
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    if(gamePlaying) {
+        // agregar el puntaje actual al puntaje global
+        scores[activePlayer] += roundScore;
+
+        //actualizar la UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+        var finalScore = document.querySelector('.final-score').value;
+        
+        if(!finalScore) {
+            finalScore = 50;
+        }
+        // console.log(finalScore);
+
+        //revisar si el jugador gano el juego
+        if (scores[activePlayer] >= finalScore) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+            document.getElementById('dice-1').style.display = 'none';
+            document.getElementById('dice-2').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        } else {
+            //siguiente jugador
+            nextPlayer();
+        }
+    }
+    
+});
+
+function nextPlayer () {
+    //siguiente jugador
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
+
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init () {
+    scores = [0,0];
+    activePlayer = 0;
+    roundScore = 0;
+    gamePlaying = true;
+
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+
+}
